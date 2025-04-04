40fffacf1db5f8a79745b9e13fc70acdf7228e14
Linh3694 <hailinh.n23@gmail.com>
2025-04-04 17:02:11 +0700

    Update students.js

--- backend/routes/students.js
+++ backend/routes/students.js
@@ -5,10 +5,12 @@ const uploadExcel = require("../middleware/excelUpload");
 const studentController = require("../controllers/studentController");
 
 router.post("/bulk", uploadExcel.single("excelFile"), studentController.bulkUploadStudents);
+router.get("/search", studentController.searchStudents);
 router.post("/", studentController.createStudent);
 router.get("/", studentController.getAllStudents);
 router.get("/:id", studentController.getStudentById);
 router.put("/:id", studentController.updateStudent);
 router.delete("/:id", studentController.deleteStudent);
+
 
 module.exports = router;
